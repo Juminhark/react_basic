311b304fa0b795932417041d34633ca7a7e7615f
Ju minhark <wnalsgkr@gmail.com>
2022-01-25 12:38:57 +0900

    useState

--- src/routes/State.js
+++ src/routes/State.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const State = () => {
-	return <div>state</div>;
+	const [count, setCount] = useState(0);
+
+	const increment = () => setCount(count + 1);
+	const decrement = () => setCount(count - 1);
+	return (
+		<div>
+			<button onClick={decrement}> - </button>
+			{count} <button onClick={increment}> + </button>
+		</div>
+	);
 };
 
 export default State;
+
+//? Class Based Component
+// import React, { Component } from 'react';
+
+// export default class State extends Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			count: 0,
+// 		};
+// 	}
+
+// 	increment = () => {
+// 		this.setState({
+// 			count: this.state.count + 1,
+// 		});
+// 	};
+
+// 	decrement = () => {
+// 		this.setState({
+// 			count: this.state.count - 1,
+// 		});
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<button onClick={this.decrement}> - </button>
+// 				{this.state.count} <button onClick={this.increment}> + </button>{' '}
+// 			</div>
+// 		);
+// 	}
+// }
