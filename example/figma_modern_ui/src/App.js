58a9e4354a170e19316e8f8e0c8e228daf2eaaa3
Ju minhark <wnalsgkr@gmail.com>
2022-03-01 20:31:47 +0900

    init

--- example/figma_modern_ui/src/App.js
+++ example/figma_modern_ui/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import {
+	Blog,
+	Features,
+	Footer,
+	Header,
+	Possibility,
+	WhatGPT3,
+} from './containers';
+
+import { Brand, CTA, Navbar } from './components';
+import './App.css';
+
 const App = () => {
 	return (
-		<div>
-			<h1>GPT-3</h1>
+		<div className="App">
+			<div className="gradient_bg">
+				<Navbar />
+				<Header />
+			</div>
+			<Brand />
+			<WhatGPT3 />
+			<Features />
+			<Possibility />
+			<CTA />
+			<Blog />
+			<Footer />
 		</div>
 	);
 };
